80ba5e5fef499ef5059907ddd932cccb4261e430
Euardo R <A01794892@tec.mx>
2024-10-08 22:20:06 -0600

    load_data

---
+++ src/load_data.py
@@ -0,0 +1,24 @@
+import argparse
+import pandas as pd
+from typing import Text
+import yaml
+
+
+def load_data(config_path: Text) -> None:
+    with open(config_path) as conf_file:
+        config = yaml.safe_load(conf_file)
+
+    data = pd.read_csv(config['data']['raw'] ,sep=',', header='infer')
+
+    data.head()
+
+    data.to_csv(config['data']['interim'], index=False)
+    
+
+if __name__ == '__main__':
+
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--config', dest='config', required=True)
+    args = args_parser.parse_args()
+
+    data = load_data(config_path=args.config)
